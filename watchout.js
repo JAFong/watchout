a8c23192b9e39fa7b4bba4dc5f77554717892c8b
Hack Reactor Students <accounts@hackreactor.com>
2015-05-22 17:31:55 -0700

    Working on Collision

--- watchout.js
+++ watchout.js
@@ -4,7 +4,7 @@ var Enemy = function() {
     this.x = randCoord();
     this.y = randCoord();
 };
-var boxWidth = 1000;
+var boxWidth = 800;
 var boxHeight = 800;
 
 var drag = d3.behavior.drag()
@@ -13,11 +13,11 @@ var drag = d3.behavior.drag()
                                             .attr('cy', d3.event.y); })
              .on('dragend', function() { circle.style('fill', 'black'); });
 
-var circle = d3.select('.board').selectAll('.draggableCircle')
+var circle = d3.select('.board').selectAll('.player')
                 .data([{ x: (boxWidth / 2), y: (boxHeight / 2), r: 25 }])
                 .enter()
                 .append('svg:circle')
-                .attr('class', 'draggableCircle')
+                .attr('class', 'player')
                 .attr('cx', function(d) { return d.x; })
                 .attr('cy', function(d) { return d.y; })
                 .attr('r', function(d) { return d.r; })
@@ -27,8 +27,8 @@ var circle = d3.select('.board').selectAll('.draggableCircle')
 
 
 var randCoord=function(){
-    return Math.floor(Math.random()*1000);
-  };
+    return Math.floor(Math.random()*800);
+  }
 
 var createEnemies= function(n){
   for (var i = 0; i < n; i++) {
@@ -59,7 +59,7 @@ var updateEnemyArray = function() {
   // Iterate through enemy array
     // run an update on each enemy
 }
-var update = function() {
+var updateEnemyPosition = function() {
   d3.select('.board').selectAll('image').data(gameEnemies)
   .transition()
   .duration(1000)
@@ -70,6 +70,45 @@ var update = function() {
     return randCoord();
   });
 };
+
+//Collision
+//Input: Array of enemies and player Output: boolean
+//Edge case: enemies colliding
+//fn
+  //iterate through enemy array
+    //check distance between current enemy and player
+    // if distance is less than enemy radius + player radius
+      //return true
+  //return false
+var distance = function(x1, y1, x2, y2) {
+  var xCalculation = x1 - x2;
+  var yCalculation = y1 - y2;
+  return Math.sqrt(xCalculation*xCalculation + yCalculation*yCalculation);
+}
+var checkCollision= function(){
+  var _enemies=d3.selectAll('.enemy');
+  var _player= d3.selectAll('.player');
+  var playerX = _player.attr('x');
+  var playerY = _player.attr('y');
+  _enemies.each(function() {
+    var enemyX = d3.select(this).attr('x');
+    var enemyY = d3.select(this).attr('y');
+    if(distance(enemyX,enemyY,playerX,playerY)<200){
+      var temp= d3.select('body').selectAll('.collisionCount').text();
+      d3.select('body').selectAll('.collisionCount').text(parseInt(temp)+1);
+      return true;
+    }
+  });
+}
+
+var testCollision= function(){
+  var _enemies=d3.select('.board').selectAll('.enemy')[0];
+  var _player= d3.selectAll('.player');
+  // console.log(_enemies.attr('y'));
+  // console.log(_player.attr('cy'));
+}
+
 createEnemies(10);
 addEnemies();
-setInterval(update, 1000);
+setInterval(updateEnemyPosition, 1000);
+setInterval(checkCollision, 100);
